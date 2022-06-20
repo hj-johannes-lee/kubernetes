cc76cf6984e2180d1308b18fa5bc4415e77ba7e4
Hyeongju Johannes Lee <hyeongju.lee@intel.com>
2022-06-20 15:13:19 +0300

    cdi example driver: implement NodePrepareResource/NodeUnprepareResource
    Signed-off-by: Hyeongju Johannes Lee <hyeongju.lee@intel.com>

--- test/integration/cdi/example-driver/server/nodeserver.go
+++ test/integration/cdi/example-driver/server/nodeserver.go
@@ -2,9 +2,13 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
+	cdiapi "github.com/container-orchestrated-devices/container-device-interface/pkg/cdi"
+	"github.com/container-orchestrated-devices/container-device-interface/specs-go"
 	"k8s.io/klog/v2"
 	cdipbv1 "k8s.io/kubernetes/pkg/kubelet/apis/cdi/v1alpha1"
 )
@@ -37,12 +41,75 @@ func (ex *exampleDriver) run() {
 	s.Wait()
 }
 
+var jsonFilePath = "/etc/cdi/vendor.json"
+var devicePath = "/dev/tty"
+var cdiVersion = "0.4.0"
+var kind = "vendor.com/device"
+var deviceName = "example"
+
 func (ex *exampleDriver) NodePrepareResource(ctx context.Context, req *cdipbv1.NodePrepareResourceRequest) (*cdipbv1.NodePrepareResourceResponse, error) {
 	klog.Infof("NodePrepareResource is called")
-	return &cdipbv1.NodePrepareResourceResponse{}, nil
+	// create CDI Files
+
+	if _, err := os.Stat(devicePath); os.IsNotExist(err) {
+		klog.Errorf(err.Error())
+		return &cdipbv1.NodePrepareResourceResponse{}, err
+	} else {
+		// create spec
+		klog.Infof("Creating CDI Files")
+
+		containerEditsInDevices := &cdiapi.ContainerEdits{}
+		containerEditsInDevices.Append(&cdiapi.ContainerEdits{ContainerEdits: &specs.ContainerEdits{
+			DeviceNodes: []*specs.DeviceNode{
+				{
+					Path: devicePath,
+					Type: "c",
+				},
+			},
+		}})
+		containerEdits := &cdiapi.ContainerEdits{}
+		containerEdits.Append(&cdiapi.ContainerEdits{ContainerEdits: &specs.ContainerEdits{
+			Env: []string{"envVar2=envVal2"},
+		}})
+
+		spec, err := cdiapi.NewSpec(
+			&specs.Spec{
+				Version: cdiVersion,
+				Kind:    kind,
+				Devices: []specs.Device{{
+					Name:           deviceName,
+					ContainerEdits: *containerEditsInDevices.ContainerEdits,
+				}},
+				ContainerEdits: *containerEdits.ContainerEdits,
+			},
+			devicePath, 1)
+		if err != nil {
+			return nil, err
+		} else {
+			klog.Infof("CDI spec is validated")
+		}
+
+		// create json file from spec
+		doc, err := json.Marshal(*spec)
+		if err != nil {
+			return nil, err
+		}
+
+		if err := ioutil.WriteFile(jsonFilePath, doc, os.FileMode(0644)); err != nil {
+			return nil, err
+		} else {
+			klog.Infof("Json file is created at " + jsonFilePath)
+		}
+
+		//return qualified name of the cdi device
+		dev := spec.GetDevice(deviceName).GetQualifiedName()
+		klog.Infof("Return: " + dev)
+		return &cdipbv1.NodePrepareResourceResponse{CdiDevice: []string{dev}}, nil
+	}
 }
 
 func (ex *exampleDriver) NodeUnprepareResource(ctx context.Context, req *cdipbv1.NodeUnprepareResourceRequest) (*cdipbv1.NodeUnprepareResourceResponse, error) {
 	klog.Infof("NodeUnprepareResource is called")
+	os.Remove(jsonFilePath)
 	return &cdipbv1.NodeUnprepareResourceResponse{}, nil
 }
